2ee4cadb2fa8010ef16151b813e042cdb3e77e1b
timblau1 <162990992+timblau1@users.noreply.github.com>
2024-03-15 13:24:42 +0100

    Pagina protocolos echa
    qwdqwdqdqw

--- Java segunda pag.js
+++ Java segunda pag.js
@@ -1,8 +1,13 @@
-// Cambiar color de fondo aleatorio al hacer click
+// Cambiar a tonos pasteles aleatorios al hacer click
 const body = document.querySelector('body');
+
 body.addEventListener('click', function() {
-  const r = Math.floor(Math.random() * 256);
-  const g = Math.floor(Math.random() * 256); 
-  const b = Math.floor(Math.random() * 256);
+
+  // Tonos más claros
+  const r = Math.floor(Math.random() * 128) + 128; 
+  const g = Math.floor(Math.random() * 128) + 128;
+  const b = Math.floor(Math.random() * 128) + 128;
+
   body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+
 });
