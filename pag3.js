9ee6c042a41cbbb77de6e4d15a8094aec3b1c3db
timblau1 <162990992+timblau1@users.noreply.github.com>
2024-03-19 12:15:09 +0100

    Pagina v1d30s p0rn0 exa (Ay Kbron)

---
+++ pag3.js
@@ -0,0 +1,8 @@
+// Al hacer clic en el video
+video.querySelector('video').play(); 
+
+// Al abrir el popup
+popup.querySelector('video').play();
+
+// Al cerrar el popup 
+popup.querySelector('video').pause();
